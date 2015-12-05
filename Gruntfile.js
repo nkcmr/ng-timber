1b7de18a6c0fda240577b5273d4f55809d7f281d
nick comer <nick@comer.io>
2015-12-05 13:51:14 -0500

    update build scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,12 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     uglify: {
       dist: {
+        options: {
+          banner: '/**\ntimber v<%= pkg.version %>\n(c) 2014-2016 Nick Comer http://nick.comer.io\nLicense: MIT\n*/\n',
+          sourceMap: true
+        },
         src: 'src/ng-timber.js',
         dest: 'dist/ng-timber.min.js'
       }
@@ -10,5 +15,5 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify')
 
-  grunt.registerTask('default', ['uglify'])
+  grunt.registerTask('default', ['uglify:dist'])
 }
