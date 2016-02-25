40caaa74ff1f785467a3cea343dd094bec18668d
nick comer <nick@comer.io>
2016-02-24 21:17:18 -0500

    update build process to use browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,37 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    curr_year: (new Date()).getUTCFullYear(),
     uglify: {
       dist: {
         options: {
-          banner: '/**\ntimber v<%= pkg.version %>\n(c) 2014-2016 Nick Comer http://nick.comer.io\nLicense: MIT\n*/\n',
+          banner: [
+            '/**',
+            ' * ng-timber v<%= pkg.version %>',
+            ' * (c) 2014 - <%= curr_year %> nick comer (@nkcmr)',
+            ' * license: mit',
+            ' */'
+          ].join('\n'),
           sourceMap: true
         },
-        src: 'src/ng-timber.js',
+        src: 'dist/ng-timber.js',
         dest: 'dist/ng-timber.min.js'
+      }
+    },
+    browserify: {
+      dist: {
+        options: {
+          ignore: ['angular']
+        },
+        files: {
+          'dist/ng-timber.js': ['src/ng-timber.js']
+        }
       }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-browserify')
 
-  grunt.registerTask('default', ['uglify:dist'])
+  grunt.registerTask('default', ['browserify:dist', 'uglify:dist'])
 }
